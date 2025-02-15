f2e6df40cc44cfe730056a48a53f8b2b5cac25a7
Steven Yan <yulong.yan@uqconnect.edu.au>
2025-02-16 00:28:10 +1100

    chore: bump version to 1.0.9

--- torrent-downloader-python/setup.py
+++ torrent-downloader-python/setup.py
@@ -10,7 +10,7 @@ else:
 
 setup(
     name="torrent-downloader-python",
-    version="1.0.8",
+    version="1.0.9",
     packages=find_packages(),
     package_data={
         'torrent_downloader': ['torrent_downloader_gui.py'],
