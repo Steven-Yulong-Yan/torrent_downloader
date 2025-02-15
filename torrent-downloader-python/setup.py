08fff07edae36d8fa7f7cb64926ffb89277af0eb
Steven Yan <yulong.yan@uqconnect.edu.au>
2025-02-16 01:42:22 +1100

    chore: bump version to 1.0.13 (Python) and 1.0.22 (React)

--- torrent-downloader-python/setup.py
+++ torrent-downloader-python/setup.py
@@ -10,7 +10,7 @@ else:
 
 setup(
     name="torrent-downloader-python",
-    version="1.0.1",
+    version="1.0.13",
     packages=find_packages(),
     install_requires=libtorrent_requires,
     entry_points={
--- torrent-downloader-python/torrent_downloader/__init__.py
+++ torrent-downloader-python/torrent_downloader/__init__.py
@@ -2,4 +2,4 @@
 Torrent Downloader - A modern, user-friendly torrent downloader application.
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.13"
